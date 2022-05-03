9bf6655f95225bb6d39a70c7ca5f79e5f45d99e9
martinlaursen97 <martin_laursen21@hotmail.com>
2022-05-03 10:00:42 +0200

    first commit

---
+++ src/main/java/com/company/barmanager/BarManagerApplication.java
@@ -0,0 +1,13 @@
+package com.company.barmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarManagerApplication.class, args);
+    }
+
+}
