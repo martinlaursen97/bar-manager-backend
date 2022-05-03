b283537ffe111d18bb1b9e264268eea4645d3046
martinlaursen97 <martin_laursen21@hotmail.com>
2022-05-03 12:08:01 +0200

    UserRepo create test

---
+++ src/test/java/com/company/barmanager/repository/UserRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.company.barmanager.repository;
+
+import com.company.barmanager.model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @BeforeEach
+    void beforeEach() {
+
+    }
+
+    @Test
+    void createUser() {
+        User user = new User();
+        user.setUsername("erik");
+        user.setPassword("123");
+
+        User createdUser = userRepository.save(user);
+        userRepository.deleteById(createdUser.getId());
+
+        Assertions.assertNotNull(createdUser);
+    }
+}
