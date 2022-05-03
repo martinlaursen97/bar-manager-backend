5983f766e53c841d639aa1e19f61b38323c4667c
AlexanderNormann <alex654v@stud.kea.dk>
2022-05-03 14:30:22 +0200

    git branch

--- src/main/java/com/company/barmanager/controller/ItemRESTController.java
+++ src/main/java/com/company/barmanager/controller/ItemRESTController.java
@@ -1,9 +1,18 @@
 package com.company.barmanager.controller;
 
+import com.company.barmanager.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
+@RequestMapping("api/item")
 public class ItemRESTController {
+
+  private final ItemService itemService;
+
+  @Autowired
+  public Item
 }
