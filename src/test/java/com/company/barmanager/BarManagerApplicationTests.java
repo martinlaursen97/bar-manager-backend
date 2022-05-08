17663b34d5afa5fe4082f004834e8daddb812fb2
martinlaursen97 <martin_laursen21@hotmail.com>
2022-05-08 23:15:19 +0200

    cicd test 2

--- src/test/java/com/company/barmanager/BarManagerApplicationTests.java
+++ src/test/java/com/company/barmanager/BarManagerApplicationTests.java
@@ -1,9 +1,10 @@
 package com.company.barmanager;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@DataJpaTest
 class BarManagerApplicationTests {
 
     @Test
--- src/test/java/com/company/barmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/company/barmanager/repository/UserRepositoryTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@DataJpaTest
 class UserRepositoryTest {
 
     @Autowired
