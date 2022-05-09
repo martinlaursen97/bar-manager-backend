b54cf0ab2f293bbc25e7657336aa279c521d8cb1
martinlaursen97 <martin_laursen21@hotmail.com>
2022-05-09 19:34:44 +0200

    cicd test 5

--- src/test/java/com/company/barmanager/BarManagerApplicationTests.java
+++ src/test/java/com/company/barmanager/BarManagerApplicationTests.java
@@ -1,7 +1,9 @@
 package com.company.barmanager;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 class BarManagerApplicationTests {
