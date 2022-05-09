ba47ca4e20634b29c262783dd589beb0ae504808
martinlaursen97 <martin_laursen21@hotmail.com>
2022-05-09 14:26:13 +0200

    cicd test 4

--- src/test/java/com/company/barmanager/service/UserServiceTest.java
+++ src/test/java/com/company/barmanager/service/UserServiceTest.java
@@ -5,13 +5,18 @@ import com.company.barmanager.model.User;
 import com.company.barmanager.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
 @DataJpaTest
+@Import(UserService.class)
 class UserServiceTest {
 
     @Autowired
@@ -19,17 +24,17 @@ class UserServiceTest {
 
 
 
-    //@Test
-    //void verifyLogin() throws LoginException {
-    //    // Arrange
-    //    User t = new User();
-    //    t.setUsername("a");
-    //    t.setPassword("b");
-//
-    //    // Act
-//
-    //    // Assert
-    //    Assertions.assertThrows(LoginException.class, () -> userService.verifyLogin(t));
-//
-    //}
+    @Test
+    void verifyLogin() throws LoginException {
+        // Arrange
+        User t = new User();
+        t.setUsername("a");
+        t.setPassword("b");
+
+        // Act
+
+        // Assert
+        Assertions.assertThrows(LoginException.class, () -> userService.verifyLogin(t));
+
+    }
 }
